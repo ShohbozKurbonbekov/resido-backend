a9273363db7ac73ab86d5d8133ac6d9dcb12a5d0
ShohbozKurbonbekov <skurbonbekov98@gmail.com>
2026-05-06 02:56:52 +0900

    fix(deploy-shell):modify deploy shell

--- deploy.sh
+++ deploy.sh
@@ -5,15 +5,4 @@ git reset --hard
 git checkout master
 git  pull origin master
 
-npm i 
-npm run build
-pm2 start process.config.js --env production
-
-
-#! DEVELOPMENT
-# git reset --hard
-# git checkout develop
-# git pull origin develop
-
-# npm i
-# pm2 start process.config.js --env development
+docker compose up -d
